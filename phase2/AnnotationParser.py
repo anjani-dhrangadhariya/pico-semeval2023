8a73fdc2c1349d76593c95c8b05b5118df908468
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-10-31 13:39:15 +0000

    read files

--- phase2/AnnotationParser.py
+++ phase2/AnnotationParser.py
@@ -12,3 +12,14 @@ import scispacy
 #loading the scispacy model
 nlp = spacy.load('en_core_sci_sm')
 
+# open file
+file_path_train = '/mnt/nas2/data/systematicReview/semeval2023/data/st2_train_inc_text.csv'
+file_path_test = '/mnt/nas2/data/systematicReview/semeval2023/data/st2_test_inc_text.csv'
+
+df_train = pd.read_csv(file_path_train, sep=',')
+train_df = df_train.to_dict('records')
+
+df_test = pd.read_csv(file_path_test, sep=',')
+test_df = df_test.to_dict('records')
+
+
