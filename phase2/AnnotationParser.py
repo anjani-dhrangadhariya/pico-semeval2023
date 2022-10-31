d2bb4d4a4409d53a6a12a58aa8d23c82b02778ea
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-10-31 13:40:15 +0000

    Function to parse char annotations

--- phase2/AnnotationParser.py
+++ phase2/AnnotationParser.py
@@ -25,3 +25,65 @@ test_df = df_test.to_dict('records')
 
 deleted_list = ['[deleted]', '[removed]', 'deleted by user']
 
+def get_char_labels(df):
+    
+    # parse dataframe and fetch annotations in a dict
+
+    labels = []
+
+    for counter, row in enumerate(df):
+        #print('--------------------------------------------------------')
+
+        reddit_id = row['subreddit_id']
+
+        post_id = row['post_id']
+        #print( post_id )
+
+        claim = row['claim']
+        #print('Claim: ', claim)
+
+        full_text = row['text']
+        #print('Full-Text: ', full_text)
+
+        if any(word in full_text for word in deleted_list):
+            # If the post was removed by the user
+            labels.append('N.A.')
+        else:
+            # If the post was not removed by the user
+            # Get entities
+            stage2_labels = ast.literal_eval( row['stage2_labels']  )
+            stage2_labels = stage2_labels[0]['crowd-entity-annotation']['entities']
+
+            # Get Char indices
+            full_text_indices = [ counter for counter, i in enumerate(full_text) ]
+
+            label_each_char = [0] * len(full_text)
+
+            for l in stage2_labels:
+                extrct_annot = row['text'][ l['startOffset'] : l['endOffset'] ]  
+                #print( 'EXTRACTED ANNOTATIONS: ' , extrct_annot, ' >>>>>>>> ' , l['label'] )
+
+                # Are the start and stop offsets in the offsets?
+                if l['startOffset'] in full_text_indices:
+
+                    prev_length = len(label_each_char)
+                    start = l['startOffset']
+                    end = l['startOffset']+(len(extrct_annot))
+                    label_indices = [ i for i in range(start, end) ]
+                    for i in range( start, end ):
+                        old_label = label_each_char[i]
+                        new_label = 1
+                        if new_label > old_label:
+                            label_each_char[i] = new_label
+                    assert len(label_each_char) == prev_length
+                    #print( label_each_char )
+
+            labels.append(label_each_char)
+
+        #if counter == 50:
+        #    break
+
+        #print('--------------------------------------------------------')
+
+    # return return 
+    return labels
