31b1ce1baee801cbc061c74842919152c78b2a4d
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-11-21 11:37:31 +0100

    feat: add cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='PICO extraction shared task for SemEval 2023',
+    author='Wojciech Kusa',
+    license='MIT',
+)
